98677c67bbf82966565e89864acc44ab98fcb72c
Gerald Rotich <58162259+geraldrotich@users.noreply.github.com>
2022-08-12 13:28:17 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
+ * print_char - Prints a character
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
